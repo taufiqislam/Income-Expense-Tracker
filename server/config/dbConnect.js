d6833be9aa723beac4ebb988e858cf29a401c2f8
cryomancer148 <taufiqislam15@gmail.com>
2024-07-29 02:37:15 +0600

    security

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,15 +1,22 @@
-const mongoose = require("mongoose");
+require('dotenv').config({ path: './.env' }); // .env file is in the server folder
 
-// connect
+const mongoose = require('mongoose');
 
-const dbConnect = async ()=>{
+const dbConnect = async () => {
     try {
-        await mongoose.connect("mongodb+srv://cryomancer148:proto148type@cluster0.wet9jqr.mongodb.net/Income-Expense?retryWrites=true&w=majority&appName=Cluster0");
-        console.log("DB connected successfully");
+        const uri = process.env.MONGODB_URI;
+        if (!uri) {
+            throw new Error('MONGODB_URI is not defined in the environment variables.');
+        }
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('DB connected successfully');
     } catch (error) {
         console.log(error.message);
         process.exit(1);
     }
-}
+};
 
 dbConnect();
